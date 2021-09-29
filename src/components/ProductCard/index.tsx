db2021228ec7e80a907fd4bb66c30a07964854ff
Victor <victorgutierrezgomes@gmail.com>
2021-09-28 22:43:39 -0400

    Implement ProductCard component

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Product } from 'types';
+import { formatCurrency } from 'utils/formatCurrency';
+import './styles.css'
+
+export const ProductCard: React.FC<Product> = ({ productImageUrl, productTitle, productOldPriceCents, productOnSalePriceCents, availableInstallmentsWithoutInterest, installmentsWithoutInterestPriceCents }) => {
+  return (
+    <div className="product-card">
+
+      {/* TODO: Container with favorite button */}
+
+      <img className="product-card__image" src={productImageUrl} alt={productTitle} />
+
+      <div className="product-card__title-container">
+        <p className="product-card__title">{productTitle}</p>
+      </div>
+
+      <div className="product-card__prices-container">
+        <p className="product-card__old-price">{formatCurrency(productOldPriceCents / 100)}</p>
+        <p className="product-card__sale-price">{formatCurrency(productOnSalePriceCents / 100)}</p>
+
+        <p className="product-card__payment">em até <b>{availableInstallmentsWithoutInterest}x de R$ {formatCurrency(installmentsWithoutInterestPriceCents / 100)}</b> sem juros</p>
+      </div>
+      {/*TODO: BUTTON COMPONENT */}
+
+    </div>
+  )
+}
