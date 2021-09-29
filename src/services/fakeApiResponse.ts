043a54dfb63b42df97c08470cba6b0540471413e
Victor <victorgutierrezgomes@gmail.com>
2021-09-28 22:38:45 -0400

    Import mocked image inside fake response

--- src/services/fakeApiResponse.ts
+++ src/services/fakeApiResponse.ts
@@ -1,23 +1,24 @@
 import { FakeAPIReponse } from 'types';
+import Monitor from '../assets/images/monitor.png'
 
 export const response: FakeAPIReponse = {
   data: {
     products: [
       {
         productTitle: "Monitor LED 27'' Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50",
-        productImageUrl: "./assets/images/monitor.png",
-        productOnSalePriceCents: 25990000,
-        productOldPriceCents: 28139900,
+        productImageUrl: Monitor,
+        productOnSalePriceCents: 259900,
+        productOldPriceCents: 281399,
         availableInstallmentsWithoutInterest: 10,
-        installmentsWithoutInterestPriceCents: 2599000
+        installmentsWithoutInterestPriceCents: 25990
       },
       {
         productTitle: "Monitor LED 27'' Gamer Curvo Samsung 2560 x 1440 QHD 260 Hz HDMI, DP, Gsync Série CRG50",
-        productImageUrl: "./assets/images/monitor.png",
-        productOnSalePriceCents: 23990000,
-        productOldPriceCents: 30000000,
+        productImageUrl: Monitor,
+        productOnSalePriceCents: 239900,
+        productOldPriceCents: 300000,
         availableInstallmentsWithoutInterest: 12,
-        installmentsWithoutInterestPriceCents: 1999100
+        installmentsWithoutInterestPriceCents: 19991
       }
     ]
   }
